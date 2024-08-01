14b4696c0b1917083c013117c8c3de15db3d7f40
Tam Vu <tamvu.blogspot@gmail.com>
2024-08-02 03:33:30 +0700

    feat: add docker for deployment

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,8 +4,8 @@ import os
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
+MONGO_URI = os.getenv('MONGO_URI', 'mongodb://db:27017/')
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'adcore')
 
 client = MongoClient(MONGO_URI)
 db = client[DATABASE_NAME]
